889ce0fa22ce8a13c290e7312df6d55fb936d1e9
iory <ab.ioryz@gmail.com>
2020-10-02 06:23:31 +0900

    [jsk_pcl_ros/ExtractIndices] Reset sync policy in destructor

--- jsk_pcl_ros/include/jsk_pcl_ros/extract_indices.h
+++ jsk_pcl_ros/include/jsk_pcl_ros/extract_indices.h
@@ -58,6 +58,20 @@ namespace jsk_pcl_ros
       sensor_msgs::PointCloud2 > ApproximateSyncPolicy;
 
     ExtractIndices(): DiagnosticNodelet("ExtractIndices") {}
+
+    ~ExtractIndices() {
+      // This fixes the following error on shutdown of the nodelet:
+      // terminate called after throwing an instance of
+      // 'boost::exception_detail::clone_impl<boost::exception_detail::error_info_injector<boost::lock_error> >'
+      //     what():  boost: mutex lock failed in pthread_mutex_lock: Invalid argument
+      // Also see ros/ros_comm#720 .
+      if (approximate_sync_) {
+        async_.reset();
+      } else {
+        sync_.reset();
+      }
+    }
+
   protected:
     virtual void onInit();
     virtual void subscribe();
